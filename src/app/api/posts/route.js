23226eaed7f49bf804a08fa8b6999e615afc2f72
ChiefTacos <seipsirc@gmail.com>
2023-06-22 15:29:50 -0500

    oh sweet Jeepers

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -1,21 +1,26 @@
 import { NextResponse } from "next/server";
-
+mongoose.set('strictQuery', false);
 import connect from "@/utils/db";
 import Post from "@/models/Post";
 
 export const GET = async (request) => {
+
   const url = new URL(request.url);
 
   const username = url.searchParams.get("username");
-
+console.log('first')
   try {
     await connect();
 
     const posts = await Post.find(username && { username });
 
     return new NextResponse(JSON.stringify(posts), { status: 200 });
+console.log('second')
+
   } catch (err) {
     return new NextResponse("Database Error", { status: 500 });
+console.log('third')
+
   }
 };
 
