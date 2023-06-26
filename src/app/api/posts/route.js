f2d9ae7071698256140608df963c274ca0b66d7f
ChiefTacos <seipsirc@gmail.com>
2023-06-26 17:36:34 -0500

    secondbig

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -6,7 +6,7 @@ export const GET = async (request) => {
 
   const url = new URL(request.url);
 
-  const username = url.searchParams.get("username");
+  // const username = url.searchParams.get("username");
 console.log('first')
   try {
     await connect();
